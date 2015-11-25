88fa9c533b6bb4b1dbd56aaa38c5ebc86ff3eb19
Nicolas HAMARD <nox.tux@gmail.com>
2015-11-25 18:56:05 -0500

    cleaning

--- inf3135/TP1/ceasar.c
+++
@@ -1,9 +0,0 @@
-#include "ceasar.h"
-
-int main (int argc, char **argv)
-{
-	FILE *fpi
-	FILE *fpo
-
-	return EXIT_SUCCESS;
-}
--- inf3135/TP1/fileIO.h
+++
@@ -1,7 +0,0 @@
-#include "ceasar.h"
-
-CEASAR_str openFile (char* path, char option);
-int closeFile (CEASAR_str cstr)
-bool write (char* input, FILE* file);
-char* read (FILE* file);
-int getFileSize(FILE* file);
